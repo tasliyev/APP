27866450235e805684aee18147952d18e7dc074c
tasliyev <157374026+tasliyev@users.noreply.github.com>
2024-02-20 15:05:36 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,105 @@
+const cacheName = 'SPA-salon-v1';
+const filesToCache = 
+[
+    '/',
+    '/index.html',
+    '/Service_1.html',
+    '/Service_2.html',
+    '/Service_3.html',
+    '/Service_4.html',
+    '/Service_5.html',
+    '/Service_6.html',
+    '/Service_7.html',
+    '/Service_8.html',
+    '/Service_9.html',
+    '/Eng_1.html',
+    '/Eng_2.html',
+    '/Eng_3.html',
+    '/Eng_4.html',
+    '/Eng_5.html',
+    '/Eng_6.html',
+    '/Eng_7.html',
+    '/Eng_8.html',
+    '/Eng_9.html',
+    '/ENG.html',
+    '/assets/css/flex-slider.css',
+    '/assets/css/fontawesome.css',
+    '/assets/css/owl.css',
+    '/assets/css/templatemo-sixteen.css',
+    '/assets/fonts/Flaticon.woff',
+    '/assets/fonts/flexslider-icon.eot',
+    '/assets/fonts/flexslider-icon.svg',
+    '/assets/fonts/flexslider-icon.ttf',
+    '/assets/fonts/flexslider-icon.woff',
+    '/assets/fonts/FontAwesome.otf',
+    '/assets/fonts/fontawesome-webfont.eot',
+    '/assets/fonts/fontawesome-webfont.svg',
+    '/assets/fonts/fontawesome-webfont.ttf',
+    '/assets/fonts/fontawesome-webfont.woff',
+    '/assets/fonts/fontawesome-webfont.woff2',
+    '/assets/fonts/slick.eot',
+    '/assets/fonts/slick.svg',
+    '/assets/fonts/slick.ttf',
+    '/assets/fonts/slick.woff',
+    '/assets/images/bg_image.jpg',
+    '/assets/images/SPA.png',
+    '/assets/js/accordions.js',
+    '/assets/js/custom.js',    
+    '/assets/js/isotope.js',
+    '/assets/js/owl.js',
+    '/assets/js/slick.js',
+    'assets/videos/1/буккальный массаж.mp4',
+    'assets/videos/1/классический массаж.mp4',
+    'assets/videos/1/кхмерский массаж медной манетой.mp4',
+    'assets/videos/1/скульптурный массаж.mp4',
+    'assets/videos/2/аппаратная чистка.mp4',
+    'assets/videos/2/механическая чистка.mp4',
+    'assets/videos/3/SPA-лица.mp4',
+    'assets/videos/3/Альгинатная маска.mp4',
+    'assets/videos/3/Фарфоровая куколка.mp4',
+    'assets/videos/4/Пилинг кожи головы.mp4',
+    'assets/videos/5/Screenrecorder-2024-02-13-13-11-29-902.mp4',
+    'assets/videos/5/Глазирование волос.mp4',
+    'assets/videos/5/Кофейный скраб волос.mp4',
+    'assets/videos/5/Чистка кожи головы.mp4',
+    'assets/videos/6/Диагностика кожи лица.mp4',
+    'assets/videos/7/SMAS AЛЬТЕРА.mp4',
+    'assets/videos/7/РФ-лифтинг(лица).mp4',
+    'assets/videos/8/SMAS лифтинг.mp4',
+    'assets/videos/8/Инфракрасный вакуум.mp4',
+    'assets/videos/9/Маникюр.mp4',
+    'assets/videos/9/Педикюр.mp4',
+    'vendor/bootstrap/css/bootstrap.css',
+    'vendor/bootstrap/css/bootstrap.min.css',
+    'vendor/bootstrap/css/bootstrap.css.map',
+    'vendor/bootstrap/css/bootstrap.min.css.map',
+    'vendor/bootstrap/js/bootstrap.bundle.js',
+    'vendor/bootstrap/js/bootstrap.bundle.js.map',
+    'vendor/bootstrap/js/bootstrap.bundle.min.js',
+    'vendor/bootstrap/js/bootstrap.bundle.min.js.map',
+    'vendor/bootstrap/js/bootstrap.js',
+    'vendor/bootstrap/js/bootstrap.js.map',
+    'vendor/bootstrap/js/bootstrap.min.js',
+    'vendor/bootstrap/js/bootstrap.min.js.map',
+    'vendor/jquery/jquery.js',
+    'vendor/jquery/jquery.min.js',
+    'vendor/jquery/jquery.min.js.map',
+    'vendor/jquery/jquery.slim.js',
+    'vendor/jquery/jquery.slim.min.js',
+    'vendor/jquery/jquery.slim.min.js.map'
+];
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
